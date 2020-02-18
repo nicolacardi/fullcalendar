ca3a9ffd27ca880b6817bc38a55a829fc167d051
Nicola Cardi <NicolaCardi@MBP-di-Nicola.homenet.telecomitalia.it>
2020-02-18 12:38:35 +0100

    18/02/2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { DialogEvent } from './app.component';
+import { DialogData } from './app.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogEvent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    FullCalendarModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatCheckboxModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    DialogEvent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+ }
+
+
+//  import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { FullCalendarModule } from '@fullcalendar/angular';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     FullCalendarModule // import the FullCalendar module! will make the FullCalendar component available
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
